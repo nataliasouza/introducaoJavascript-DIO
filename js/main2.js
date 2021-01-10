fa40bb2ffec5a38b623a36ef3410e0b2d32b5450
Natália Souza <nataliasouza.ti@hotmail.com>
2021-01-09 23:53:43 -0300

    Parte 2 - Aula 4 | Manipulando elementos da página

--- js/main2.js
+++ js/main2.js
@@ -1,5 +1,34 @@
-//Aula 04 - Desenvolva páginas web com javascript
+function clicou() {
+    document.getElementById("agradecimento").innerHTML = "Obrigada por clicar";
+    //console.log(document.getElementById("agradecimento"));
+}
 
+function redirecionar() {
+    window.open("https://digitalinnovation.one/");
+    //window.location.href = "https://digitalinnovation.one/";
+}
+
+function trocar(elemento) {
+    //document.getElementById("mousemove").innerHTML = "Obrigada por passar o mouse";
+    //alert("trocar texto");
+    elemento.innerHTML = "Obrigada por passar o mouse";
+}
+
+function voltar(elemento) {
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+    elemento.innerHTML = "Passe o mouse aqui";
+}
+
+function load() {
+    alert("Página Carregada");
+}
+
+function funcaoChange(elemento) {
+    console.log(elemento.value);
+}
+
+//Aula 04 - Desenvolva páginas web com javascript
+/*
 function soma(n1, n2) {
     return n1 + n2;
 }
@@ -15,6 +44,6 @@ function validaIdade(idade){
 }
 
 var idade = prompt("Qual a sua idade");
-console.log(validaIdade(idade));
+console.log(validaIdade(idade));*/
 
 
