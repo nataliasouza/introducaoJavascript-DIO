598a6922468072f192de28c20932852edd22129f
Natália Souza <nataliasouza.ti@hotmail.com>
2021-01-09 21:15:41 -0300

    Aula 01 - Introdução ao Javascript

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var nome = "Natália Souza";
+var idade = 36;
+var idade2 = 1;
+var frase = "Alemanha é o melhor time do mundo.";
+//alert("Bem vinda " + nome + " você tem: " + idade);
+//alert(idade + idade2)
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Alemanha", "Brasil"));
+//alert(frase.replace("Argentina", "Brasil"));
+console.log(frase.toLocaleUpperCase());
