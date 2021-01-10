c03a23c66c4380891fdb1875ba2e79cc935baf1c
Natália Souza <nataliasouza.ti@hotmail.com>
2021-01-09 21:41:23 -0300

    Aula 02 - Array e Dicionário

--- js/main.js
+++ js/main.js
@@ -1,11 +1,45 @@
-var nome = "Natália Souza";
+//Aula 01
+/*var nome = "Natália Souza";
 var idade = 36;
 var idade2 = 1;
 var frase = "Alemanha é o melhor time do mundo.";
-//alert("Bem vinda " + nome + " você tem: " + idade);
-//alert(idade + idade2)
+alert("Bem vinda " + nome + " você tem: " + idade);
+alert(idade + idade2)
 console.log(nome);
 console.log(idade + idade2);
 console.log(frase.replace("Alemanha", "Brasil"));
-//alert(frase.replace("Argentina", "Brasil"));
-console.log(frase.toLocaleUpperCase());
+alert(frase.replace("Argentina", "Brasil"));
+console.log(frase.toLocaleUpperCase());*/
+
+//Aula02
+/*var lista = ["maça", "pêra", "laranja"]
+//push adiciona um elemento ao final do array
+lista.push("mamão");
+console.log(lista);
+
+//pop remove o último elemento do array
+lista.pop();
+console.log(lista);
+
+//mostrando o elemento da posicao 0 do array
+console.log(lista[0]);
+
+//mostrar a quantidade de elemento do array 
+console.log(lista.length);
+
+//mostra os elementos do array em ordem invertida
+console.log(lista.reverse());
+
+//mostrando os elementos do array como String em
+console.log(lista.toString());
+
+//colocar espaço e ou traço, barra e etc, no lugar da vírgula
+console.log(lista.join(" - "));*/
+
+/*var fruta = {nome: "maçã", cor: "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor); */
+
+var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
